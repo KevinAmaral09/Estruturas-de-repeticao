56bfb4bd7586de5dd543c0d03f0103127680bd03
Kevin <kevinamaral65@gmail.com>
2021-05-06 16:40:42 -0300

    Estruturas de repetição sem o desafio

---
+++ Exercicio 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Estruturas_de_repetição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número entre 0 e 10");
+            int numero = int.Parse(Console.ReadLine());
+
+            while (numero>10)
+            {
+                Console.WriteLine("Esse valor é inválido, por favor, insira um valor válido novamente");
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            while (numero<0)
+            {
+                Console.WriteLine("Esse valor é inválido, por favor, insira um valor válido novamente");
+                numero = int.Parse(Console.ReadLine()); 
+            }           
+        }
+    }
+}
